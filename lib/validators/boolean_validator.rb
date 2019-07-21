a5777dcbde008a3832b736858ed8f5e53932d78c
Matthew Schultz <abstractapproach@gmail.com>
2019-07-21 13:26:58 -0500

    cleanup boolean validator

--- lib/validators/boolean_validator.rb
+++ lib/validators/boolean_validator.rb
@@ -1,5 +1,14 @@
 class BooleanValidator < PluginValidator
   def validate_each(record, attribute, value)
-    record.errors.add attribute, 'is not a valid boolean' unless (!options[:strict] && value.present?) || [true, false].include?(value) || (options[:allow_nil] && value.nil?)
+    record.errors.add attribute, message_or('is not a valid boolean') unless is_valid?(value)
+  end
+
+  protected
+
+  def is_valid?(value)
+    return true if !options[:strict] && value.present?
+    return true if [true, false].include?(value)
+    return true if allows_nil? && value.nil?
+    return false
   end
 end
